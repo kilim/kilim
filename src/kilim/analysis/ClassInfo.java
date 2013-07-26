acbb1f587d5c943e73540236183c8ac9466a2014
Jason Pell <jason@pellcorp.com>
2013-07-26 17:16:04 +1000

    simplify up equals and hashCode

--- src/kilim/analysis/ClassInfo.java
+++ src/kilim/analysis/ClassInfo.java
@@ -10,46 +10,37 @@ public class ClassInfo {
      * fully qualified classname in a format suitable for Class.forName
      */
     public String className;
-    
+
     /**
      * bytecode for the class
      */
     public byte[] bytes;
 
     public ClassInfo(String aClassName, byte[] aBytes) {
-      className = aClassName.replace('/', '.');
-//        className = aClassName.replace('.', '/');
+        className = aClassName.replace('/', '.');
+        // className = aClassName.replace('.', '/');
         bytes = aBytes;
     }
-    
+
     @Override
     public String toString() {
         return className;
     }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + className.hashCode();
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        return className.hashCode();
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (!(obj instanceof ClassInfo)) {
-			return false;
-		}
-		ClassInfo other = (ClassInfo) obj;
-		if (!className.equals(other.className)) {
-			return false;
-		}
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if ((obj instanceof ClassInfo) 
+            && ((ClassInfo)obj).className.equals(this.className)) {
+            return true;
+        }
+        return false;
+    }
 }
