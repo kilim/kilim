2e16c991487f87299729d8b30cc78395ed22d473
Avinash <avinash.lakshman@gmail.com>
2013-12-09 18:08:34 -0800

    moved away from the original kilim scheduler to a mix of disruptor and multicast style event dispatch

--- src/kilim/KilimEvent.java
+++ src/kilim/KilimEvent.java
@@ -12,7 +12,13 @@ class KilimEvent
 		}
 	};
 	
+	protected static long cacheLinePadding(KilimEvent kEvent)
+	{
+		return kEvent.l1 + kEvent.l2 + kEvent.l3 + kEvent.l4 + kEvent.l5 + kEvent.l6 + kEvent.l7; 
+	}
+	
 	private int tid_;	
+	protected volatile long l1, l2, l3, l4, l5, l6, l7 = 7L;
 	private Task task_;
 	
 	public int getTid()
