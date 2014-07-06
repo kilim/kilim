daf55fb6e252a52c8d2bcb466857deeafed5207f
nilangshah <shahnilang@cse.iitb.ac.in>
2014-07-05 19:56:55 -0700

    -TestMailbox1p1c for single producer single consumer added -PaddedEventSubscriber as helper class to add padding to Eventsubscriber object -UnsafeAccess class to get unsafe object -VolatileLongCell for padded volatile long

---
+++ src/kilim/PaddedEventSubscriber.java
@@ -0,0 +1,21 @@
+package kilim;
+
+
+abstract class EventSubCellPrePad{long p0,p1,p2,p3,p4,p5,p6;}
+abstract class EventSubCellValue extends EventSubCellPrePad {
+    protected EventSubscriber value;
+}
+public class PaddedEventSubscriber extends EventSubCellValue{
+    int i0;
+    long p10,p11,p12,p13,p14,p15,p16;
+    public PaddedEventSubscriber(){
+        
+    }
+    public EventSubscriber get(){
+        return value;
+    }
+    public void set(EventSubscriber e){
+        value =e;
+    }
+
+}
---
+++ src/kilim/UnsafeAccess.java
@@ -0,0 +1,19 @@
+package kilim;
+
+import java.lang.reflect.Field;
+
+import sun.misc.Unsafe;
+
+public class UnsafeAccess {
+	public static final Unsafe UNSAFE;
+	static {
+		try {
+			Field field = Unsafe.class.getDeclaredField("theUnsafe");
+			field.setAccessible(true);
+			UNSAFE = (Unsafe) field.get(null);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
