a352bc26ae2be84e11d24c7fcf64d7131c55957c
lytles <seth_lytle@yahoo.com>
2014-09-11 21:01:25 -0400

    srl.java8 -- example of the IncompatibleClassChangeError

---
+++ examples/kilim/examples/Userdata.java
@@ -0,0 +1,34 @@
+package kilim.examples;
+
+import kilim.Pausable;
+import kilim.Task;
+
+/*
+this class fails with:
+java.lang.IncompatibleClassChangeError: Found class kilim.examples.Userdata$Eats1, but interface was expected
+	at kilim.examples.Userdata.$shim$1(Userdata.java)
+	at kilim.examples.Userdata.execute(Userdata.java:27)
+	at kilim.Task._runExecute(Task.java:435)
+	at kilim.WorkerThread.run(WorkerThread.java:32)
+*/
+public class Userdata extends Task {
+    Eats1 eats1 = new Eats1();
+    Eats2 eats2 = new Eats2();
+    
+    public static class Eats1 {
+        public void insert(int kfood) throws Pausable {}
+    }
+    public static class Eats2 {
+        public void insert(int kfood) throws Pausable {}
+        public void insert2(int kfood) throws Pausable {}
+    }
+    public void execute() throws kilim.Pausable {
+        eats2.insert(0);
+        eats1.insert(0); // gets converted to $shim$1 (i'm guessing this is a lambda)
+    }
+
+    public static void main(String [] args) {
+        new Userdata().start();
+    }
+    
+}
