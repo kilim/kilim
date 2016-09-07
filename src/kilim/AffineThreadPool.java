0d93a3b40cd03ed9d7305f5a03a7865245a4c418
lytles@takashi <srl.source@nqzero.com>
2016-09-06 21:23:11 -0400

    Scheduler - fix indentation and some minor cleanup (no functional changes)

--- src/kilim/AffineThreadPool.java
+++ src/kilim/AffineThreadPool.java
@@ -3,10 +3,7 @@ package kilim;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -16,172 +13,169 @@ import java.util.concurrent.atomic.AtomicInteger;
 import kilim.timerservice.TimerService;
 
 public class AffineThreadPool {
-	private static final int MAX_QUEUE_SIZE = 4096;
-	private static final String colon_ = ":";
+    private static final int MAX_QUEUE_SIZE = 4096;
+    private static final String colon_ = ":";
 
-	protected static int getCurrentThreadId() {
-		String name = Thread.currentThread().getName();
-                
-		int sIndex = name.indexOf(colon_);
-		try { 
-                    return Integer.parseInt(name.substring(sIndex + 1, name.length()));
-                }
-                catch (Exception ex) {}
-                return name.hashCode();
-	}
+    protected static int getCurrentThreadId() {
+        String name = Thread.currentThread().getName();
 
-	private int nThreads_;
-	private String poolName_;
-	private AtomicInteger currentIndex_ = new AtomicInteger(0);
-	private List<BlockingQueue<Runnable>> queues_ = new ArrayList<BlockingQueue<Runnable>>();
-	private List<KilimStats> queueStats_ = new ArrayList<KilimStats>();
-	private List<KilimThreadPoolExecutor> executorService_ = new ArrayList<KilimThreadPoolExecutor>();
-        private AtomicInteger count = new AtomicInteger();
+        int sIndex = name.indexOf(colon_);
+        try {
+            return Integer.parseInt(name.substring(sIndex+1,name.length()));
+        } catch (Exception ex) {
+        }
+        return name.hashCode();
+    }
 
-	public AffineThreadPool(int nThreads, String name, TimerService timerService) {
-		this(nThreads, MAX_QUEUE_SIZE, name, timerService);
-	}
+    private int nThreads_;
+    private String poolName_;
+    private AtomicInteger currentIndex_ = new AtomicInteger(0);
+    private List<BlockingQueue<Runnable>> queues_ = new ArrayList<BlockingQueue<Runnable>>();
+    private List<KilimStats> queueStats_ = new ArrayList<KilimStats>();
+    private List<KilimThreadPoolExecutor> executorService_ = new ArrayList<KilimThreadPoolExecutor>();
+    private AtomicInteger count = new AtomicInteger();
 
-	public AffineThreadPool(int nThreads, int queueSize, String name,
-			TimerService timerservice) {
-		nThreads_ = nThreads;
-		poolName_ = name;
+    public AffineThreadPool(int nThreads,String name,TimerService timerService) {
+        this(nThreads,MAX_QUEUE_SIZE,name,timerService);
+    }
 
-		for (int i = 0; i < nThreads; ++i) {
-			String threadName = name + colon_ + i;
-			BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>(
-					queueSize);
-			queues_.add(queue);
+    public AffineThreadPool(int nThreads,int queueSize,String name,
+            TimerService timerservice) {
+        nThreads_ = nThreads;
+        poolName_ = name;
 
-			KilimThreadPoolExecutor executorService = new KilimThreadPoolExecutor(
-					i, 1, queue, new ThreadFactoryImpl(threadName),
-					timerservice);
-			executorService_.add(executorService);
+        for (int i = 0; i<nThreads; ++i) {
+            String threadName = name+colon_+i;
+            BlockingQueue<Runnable> queue = new LinkedBlockingQueue<Runnable>(
+                    queueSize);
+            queues_.add(queue);
 
-			queueStats_.add(new KilimStats(12, "num"));
-		}
-                timerservice.defaultExec = executorService_.get(0);
-	}
+            KilimThreadPoolExecutor executorService = new KilimThreadPoolExecutor(
+                    i,1,queue,new ThreadFactoryImpl(threadName),
+                    timerservice);
+            executorService_.add(executorService);
 
-        /** 
-         * are the queues empty
-         * allows false positives, but not false negatives
-         * ie, if this method returns false, then at some moment during the call at least one queue was non-empty
-         * if it returns true then for each queue there was a moment during the call when it was empty
-         */
-	public boolean isEmptyish() {
-		for (BlockingQueue<Runnable> queue : queues_)
-			if (! queue.isEmpty()) return false;
+            queueStats_.add(new KilimStats(12,"num"));
+        }
+        timerservice.defaultExec = executorService_.get(0);
+    }
+
+    /**
+     * are the queues empty allows false positives, but not false negatives ie, if this method returns false, then
+     * at some moment during the call at least one queue was non-empty if it returns true then for each queue there
+     * was a moment during the call when it was empty
+     */
+    public boolean isEmptyish() {
+        for (BlockingQueue<Runnable> queue : queues_)
+            if (!queue.isEmpty()) return false;
+        return true;
+    }
+
+    long getTaskCount() {
+        long totalRemainingCapacity = 0L;
+        for (BlockingQueue<Runnable> queue : queues_)
+            totalRemainingCapacity += queue.size();
+        return totalRemainingCapacity;
+    }
+
+    private int getNextIndex() {
+        int value = 0, newValue = 0;
+        do {
+            value = currentIndex_.get();
+            newValue = ((value!=Integer.MAX_VALUE) ? (value+1) : 0);
+        } while (!currentIndex_.compareAndSet(value,newValue));
+        return (newValue)%nThreads_;
+    }
+
+    public int publish(Task task) {
+        int index = getNextIndex();
+        task.setTid(index);
+        return publish(index,task);
+    }
+
+    public int publish(int index,Task task) {
+        KilimThreadPoolExecutor executorService = executorService_.get(index);
+        count.incrementAndGet();
+        executorService.submit(task);
+        queueStats_.get(index).record(executorService.getQueueSize());
+        return index;
+    }
+
+    public String getQueueStats() {
+        String statsStr = "";
+        for (int i = 0; i<queueStats_.size(); ++i)
+            statsStr += queueStats_.get(i).dumpStatistics(
+                    poolName_+":QUEUE-SZ-"+i);
+        return statsStr;
+    }
+
+    /*
+        
+     wait till there are no pending timers
+     no running tasks
+     no tasks waiting to be run
+        
+     */
+    public boolean waitIdle(TimerService ts,int delay) {
+        while (!Thread.interrupted()) {
+            if (resolved(ts))
                 return true;
-	}
-
-        long getTaskCount() {
-		long totalRemainingCapacity = 0L;
-		for (BlockingQueue<Runnable> queue : queues_) {
-			totalRemainingCapacity += queue.size();
-		}
-		return totalRemainingCapacity;
-	}
-
-	private int getNextIndex() {
-		int value = 0, newValue = 0;
-		do {
-			value = currentIndex_.get();
-			newValue = ((value != Integer.MAX_VALUE) ? (value + 1) : 0);
-		} while (!currentIndex_.compareAndSet(value, newValue));
-		return (newValue) % nThreads_;
-	}
-
-	public int publish(Task task) {
-		int index = getNextIndex();
-		task.setTid(index);
-		return publish(index, task);
-	}
-
-        
-	public int publish(int index, Task task) {
-		KilimThreadPoolExecutor executorService = executorService_.get(index);
-                count.incrementAndGet();
-		executorService.submit(task);
-		queueStats_.get(index).record(executorService.getQueueSize());
-		return index;
-	}
-
-	public String getQueueStats() {
-		String statsStr = "";
-		for (int i = 0; i < queueStats_.size(); ++i) {
-			statsStr += queueStats_.get(i).dumpStatistics(
-					poolName_ + ":QUEUE-SZ-" + i);
-		}
-		return statsStr;
-	}
-
-        
-        /*
-        
-        wait till there are no pending timers
-        no running tasks
-        no tasks waiting to be run
-        
-        */
-	public boolean waitIdle(TimerService ts,int delay) {
-            while (! Thread.interrupted()) {
-                if (resolved(ts))
-                    return true;
-                try { Thread.sleep(delay); } catch (InterruptedException ex) { break; }
-            }
-            return false;
-	}
-
-
-        private boolean resolved(TimerService ts) {
-            if (count.get() > 0) return false;
-            KilimThreadPoolExecutor exe = executorService_.get(0);
-            return ts.isEmptyLazy(exe);
+            try { Thread.sleep(delay); } catch (InterruptedException ex) { break; }
         }
-        
-        public void shutdown() {
-		for (ExecutorService executorService : executorService_) {
-			executorService.shutdown();
-		}
-	}
-        public static void publish(ThreadPoolExecutor executor,Runnable payload) {
-            KilimThreadPoolExecutor exe = (KilimThreadPoolExecutor) executor;
-            exe.count().incrementAndGet();
-            executor.getQueue().add(payload);
-        }
-        public static boolean isEmptyProxy(ThreadPoolExecutor executor) {
-            KilimThreadPoolExecutor exe = (KilimThreadPoolExecutor) executor;
-            return exe.count().get()==0;
+        return false;
+    }
+
+    private boolean resolved(TimerService ts) {
+        if (count.get()>0) return false;
+        KilimThreadPoolExecutor exe = executorService_.get(0);
+        return ts.isEmptyLazy(exe);
+    }
+
+    public void shutdown() {
+        for (ExecutorService executorService : executorService_)
+            executorService.shutdown();
+    }
+
+    public static void publish(ThreadPoolExecutor executor,Runnable payload) {
+        KilimThreadPoolExecutor exe = (KilimThreadPoolExecutor) executor;
+        exe.count().incrementAndGet();
+        executor.getQueue().add(payload);
+    }
+
+    public static boolean isEmptyProxy(ThreadPoolExecutor executor) {
+        KilimThreadPoolExecutor exe = (KilimThreadPoolExecutor) executor;
+        return exe.count().get()==0;
+    }
+
+    class KilimThreadPoolExecutor extends ThreadPoolExecutor {
+        int id = 0;
+        private TimerService timerService;
+        private BlockingQueue<Runnable> queue;
+
+        private AtomicInteger count() {
+            return count;
         }
 
-class KilimThreadPoolExecutor extends ThreadPoolExecutor {
-	int id = 0;
-	private TimerService timerService;
-	private BlockingQueue<Runnable> queue;
+        KilimThreadPoolExecutor(int id,int nThreads,
+                BlockingQueue<Runnable> queue,ThreadFactory tFactory,
+                TimerService timerService) {
+            super(nThreads,nThreads,Integer.MAX_VALUE,TimeUnit.MILLISECONDS,
+                    queue,tFactory);
+            this.id = id;
+            this.queue = queue;
+            this.timerService = timerService;
+        }
 
-        private AtomicInteger count() { return count; }
+        protected void afterExecute(Runnable r,Throwable th) {
+            super.afterExecute(r,th);
+            timerService.trigger(this);
+            count.decrementAndGet();
 
-	KilimThreadPoolExecutor(int id, int nThreads,
-			BlockingQueue<Runnable> queue, ThreadFactory tFactory,
-			TimerService timerService) {
-		super(nThreads, nThreads, Integer.MAX_VALUE, TimeUnit.MILLISECONDS,
-				queue, tFactory);
-		this.id = id;
-		this.queue = queue;
-		this.timerService = timerService;
-	}
+        }
 
-	protected void afterExecute(Runnable r, Throwable th) {
-		super.afterExecute(r, th);
-		timerService.trigger(this);
-                count.decrementAndGet();
-                
-	}
+        protected int getQueueSize() {
+            return super.getQueue().size();
+        }
 
-	protected int getQueueSize() {
-		return super.getQueue().size();
-	}
-
-}
+    }
 }
--- src/kilim/Scheduler.java
+++ src/kilim/Scheduler.java
@@ -3,172 +3,155 @@
  * You may distribute this software under the terms of the license 
  * specified in the file "License"
  */
-
 package kilim;
 
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.PriorityBlockingQueue;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import kilim.concurrent.MPSCQueue;
-import kilim.nio.NioSelectorScheduler;
 import kilim.nio.NioSelectorScheduler.RegistrationTask;
 import kilim.timerservice.Timer;
-import kilim.timerservice.TimerPriorityHeap;
 import kilim.timerservice.TimerService;
 
 /**
- * This is a basic FIFO Executor. It maintains a list of runnable tasks and
- * hands them out to WorkerThreads. Note that we don't maintain a list of all
- * tasks, but we will at some point when we introduce monitoring/watchdog
- * services. Paused tasks are not GC'd because their PauseReasons ought to be
- * registered with some other live object.
- * 
+ * This is a basic FIFO Executor. It maintains a list of runnable tasks and hands them out to WorkerThreads. Note
+ * that we don't maintain a list of all tasks, but we will at some point when we introduce monitoring/watchdog
+ * services. Paused tasks are not GC'd because their PauseReasons ought to be registered with some other live
+ * object.
+ *
  */
 public class Scheduler {
-	private static final String defaultName_ = "KilimWorker";
-	private static final int defaultQueueSize_ = Integer.MAX_VALUE;
-	public static volatile Scheduler defaultScheduler = null;
-	public static int defaultNumberThreads;
-	private static final String dash_ = "-";
-	private static final ThreadLocal<Task> taskMgr_ = new ThreadLocal<Task>();
-	private static ConcurrentMap<String, AtomicInteger> nameGenerator_ = new ConcurrentHashMap<String, AtomicInteger>();
+    private static final String defaultName_ = "KilimWorker";
+    private static final int defaultQueueSize_ = Integer.MAX_VALUE;
+    public static volatile Scheduler defaultScheduler = null;
+    public static int defaultNumberThreads;
+    private static final String dash_ = "-";
+    private static final ThreadLocal<Task> taskMgr_ = new ThreadLocal<Task>();
+    private static ConcurrentMap<String,AtomicInteger> nameGenerator_ = new ConcurrentHashMap<String,AtomicInteger>();
 
-	private String name_;
-        private int numThreads;
-	private AffineThreadPool affinePool_;
-	protected AtomicBoolean shutdown = new AtomicBoolean(false);
+    private String name_;
+    private int numThreads;
+    private AffineThreadPool affinePool_;
+    protected AtomicBoolean shutdown = new AtomicBoolean(false);
 
-	// Added for new Timer service
-	private TimerService timerService;
-	public AtomicInteger doSpinning = new AtomicInteger(0);
-	static {
-		String s = System.getProperty("kilim.Scheduler.numThreads");
-		if (s != null) {
-			try {
-				defaultNumberThreads = Integer.parseInt(s);
-			} catch (Exception e) {
-			}
-		}
-		if (defaultNumberThreads == 0) {
-			defaultNumberThreads = Runtime.getRuntime().availableProcessors();
-		}
-	}
+    // Added for new Timer service
+    private TimerService timerService;
+    public AtomicInteger doSpinning = new AtomicInteger(0);
 
-	protected static Task getCurrentTask() {
-		return taskMgr_.get();
-	}
+    static {
+        String s = System.getProperty("kilim.Scheduler.numThreads");
+        if (s!=null)
+            try {
+                defaultNumberThreads = Integer.parseInt(s);
+            } catch (Exception e) {
+            }
+        if (defaultNumberThreads==0)
+            defaultNumberThreads = Runtime.getRuntime().availableProcessors();
+    }
 
-	protected static void setCurrentTask(Task t) {
-		taskMgr_.set(t);
-	}
+    protected static Task getCurrentTask() {
+        return taskMgr_.get();
+    }
 
-	protected Scheduler() {
-	}
+    protected static void setCurrentTask(Task t) {
+        taskMgr_.set(t);
+    }
 
-	public Scheduler(int numThreads) {
-		this(numThreads, defaultQueueSize_, defaultName_);
-	}
+    protected Scheduler() {
+    }
 
-	public Scheduler(int numThreads, int queueSize, String name) {
-		name_ = name;
-		nameGenerator_.putIfAbsent(name_, new AtomicInteger());
-		timerService = new TimerService();
-		affinePool_ = new AffineThreadPool(numThreads, queueSize, name, timerService);
-                this.numThreads = numThreads;
-	}
+    public Scheduler(int numThreads) {
+        this(numThreads,defaultQueueSize_,defaultName_);
+    }
 
-	public boolean isEmptyish() {
-		return affinePool_.isEmptyish();
-	}
-	public long getTaskCount() {
-		return affinePool_.getTaskCount();
-	}
+    public Scheduler(int numThreads,int queueSize,String name) {
+        name_ = name;
+        nameGenerator_.putIfAbsent(name_,new AtomicInteger());
+        timerService = new TimerService();
+        affinePool_ = new AffineThreadPool(numThreads,queueSize,name,timerService);
+        this.numThreads = numThreads;
+    }
 
-        public int numThreads() { return numThreads; }
+    public boolean isEmptyish() {
+        return affinePool_.isEmptyish();
+    }
+
+    public long getTaskCount() {
+        return affinePool_.getTaskCount();
+    }
+
+    public int numThreads() { return numThreads; }
         
-	protected String getName() {
-		return name_;
-	}
+    protected String getName() {
+        return name_;
+    }
 
-	protected String getNextName() {
-		AtomicInteger counter = nameGenerator_.get(name_);
-		return name_ + dash_ + counter.incrementAndGet();
-	}
+    protected String getNextName() {
+        AtomicInteger counter = nameGenerator_.get(name_);
+        return name_+dash_+counter.incrementAndGet();
+    }
 
-	/**
-	 * Schedule a task to run. It is the task's job to ensure that it is not
-	 * scheduled when it is runnable.
-	 */
-	public void schedule(Task t) {
-            if (t instanceof RegistrationTask)
-                ((RegistrationTask) t).wake();
-            else
-		affinePool_.publish(t);
-	}
+    /**
+     * Schedule a task to run. It is the task's job to ensure that it is not scheduled when it is runnable.
+     */
+    public void schedule(Task t) {
+        if (t instanceof RegistrationTask)
+            ((RegistrationTask) t).wake();
+        else
+            affinePool_.publish(t);
+    }
 
-	public void schedule(int index, Task t) {
-            if (t instanceof RegistrationTask)
-                assert(false);
-            else
-                affinePool_.publish(index, t);
-	}
-    
-	public void scheduleTimer(Timer t){
-            timerService.submit(t);
-	}
+    public void schedule(int index,Task t) {
+        if (t instanceof RegistrationTask)
+            assert (false);
+        else
+            affinePool_.publish(index,t);
+    }
 
-	public void waitIdle() {
-            if (affinePool_ != null)
-                affinePool_.waitIdle(timerService,100);
-	}
-        /** 
-         * block the thread till a moment at which all scheduled tasks have completed
-         * and then shutdown the scheduler 
-         * does not prevent scheduling new tasks (from other threads) until the shutdown is complete
-         * so such a task could be partially executed
-         */
-	public void idledown() {
-            if (affinePool_ != null && affinePool_.waitIdle(timerService,100))
-                shutdown();
-	}
-        
-	public void shutdown() {
-		shutdown.set(true);
-		if (defaultScheduler == this) {
-			defaultScheduler = null;
-		}
-		if (affinePool_ != null) affinePool_.shutdown();
-                timerService.shutdown();
-	}
+    public void scheduleTimer(Timer t) {
+        timerService.submit(t);
+    }
 
-	public boolean isShutdown() {
-		return shutdown.get();
-	}
+    public void waitIdle() {
+        if (affinePool_!=null)
+            affinePool_.waitIdle(timerService,100);
+    }
 
-	public String getSchedulerStats() {
-		return affinePool_.getQueueStats();
-	}
+    /**
+     * block the thread till a moment at which all scheduled tasks have completed and then shutdown the scheduler
+     * does not prevent scheduling new tasks (from other threads) until the shutdown is complete so such a task
+     * could be partially executed
+     */
+    public void idledown() {
+        if (affinePool_!=null&&affinePool_.waitIdle(timerService,100))
+            shutdown();
+    }
 
-	public synchronized static Scheduler getDefaultScheduler() {
-		if (defaultScheduler == null) {
-			defaultScheduler = new Scheduler(defaultNumberThreads);
-		}
-		return defaultScheduler;
-	}
+    public void shutdown() {
+        shutdown.set(true);
+        if (defaultScheduler==this)
+            defaultScheduler = null;
+        if (affinePool_!=null) affinePool_.shutdown();
+        timerService.shutdown();
+    }
 
-	public static void setDefaultScheduler(Scheduler s) {
-		defaultScheduler = s;
-	}
+    public boolean isShutdown() {
+        return shutdown.get();
+    }
 
-//	public Runnable getTask(int id) {
-//		return affinePool_.stealTask(id);
-//
-//	}
+    public String getSchedulerStats() {
+        return affinePool_.getQueueStats();
+    }
+
+    public synchronized static Scheduler getDefaultScheduler() {
+        if (defaultScheduler==null)
+            defaultScheduler = new Scheduler(defaultNumberThreads);
+        return defaultScheduler;
+    }
+
+    public static void setDefaultScheduler(Scheduler s) {
+        defaultScheduler = s;
+    }
 
 }
