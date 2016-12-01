64f0f742a7a2cb3335f767ea5ee298c8bffd71dc
lytles@takashi <srl.source@nqzero.com>
2016-12-01 13:15:37 -0500

    kilim-demo-jetty - add an example of using async IO (reads only)

---
+++ demos/jetty/src/main/java/kilim/demo/KilimAsyncIO.java
@@ -0,0 +1,76 @@
+package kilim.demo;
+
+import java.io.IOException;
+import javax.servlet.AsyncContext;
+import javax.servlet.ReadListener;
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import kilim.Mailbox;
+import kilim.Pausable;
+import kilim.Task;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+
+
+public class KilimAsyncIO extends AbstractHandler implements ReadListener {
+    Iface handler;
+    private ServletInputStream in;
+    private byte[] buffer = new byte[1024];
+    public KilimAsyncIO(Iface handler) { this.handler = handler; }
+    Mailbox<String> box = new Mailbox<String>();
+
+    AsyncContext async;    
+    
+    public void handle(String target,Request br,HttpServletRequest req,HttpServletResponse resp) throws IOException, ServletException {
+        async = req.startAsync();
+        in = req.getInputStream();
+        in.setReadListener(this);
+        new kilim.Task() {
+            public void execute() throws Pausable, Exception {
+                box.get();
+                Task.sleep(1000);
+                try {
+                    String result = handler.handle(target,br,req,resp);
+                    if (result != null) resp.getOutputStream().print(result);
+                }
+                catch (Exception ex) { resp.sendError(500,"the server encountered an error"); }
+                br.setHandled(true);
+                async.complete();
+            }
+        }.start();
+    }
+
+    
+    public interface Iface {
+        String handle(String target,Request br,HttpServletRequest req,HttpServletResponse resp) throws Pausable, Exception;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(9104);
+        server.setHandler(new KilimAsyncIO((target,raw,req,resp) -> {
+            return "cruel world";
+        }));
+        server.start();
+    }
+
+    
+    public void onDataAvailable() throws IOException {
+        while (in.isReady()) {
+            int len = in.read(buffer);
+            if (len>0) {}
+            if (in.isFinished()) { break; }
+        }
+    }
+
+    public void onAllDataRead() throws IOException {
+        box.putb("hello");
+    }
+
+    public void onError(Throwable t) {
+    }
+    
+}
