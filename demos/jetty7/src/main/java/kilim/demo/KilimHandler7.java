f96867ed67797b670f87503a1ff5cc1a658cd287
lytles@takashi <srl.source@nqzero.com>
2018-01-26 14:00:01 -0500

    java7 - add an example using an older java 7 compatable version of jetty, ie 9.2

---
+++ demos/jetty7/src/main/java/kilim/demo/KilimHandler7.java
@@ -0,0 +1,33 @@
+package kilim.demo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import kilim.Pausable;
+import kilim.Task;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+
+
+/*
+    kilim uses provided-scope for it's jetty support using jetty 9.3, which is java 8+ only
+    this example project shows that kilim can be used with an older jetty with java 7
+*/
+
+
+/** example of using kilim's JettyHandler and jetty on java 7 - it works, though less elegant without lambdas */
+public class KilimHandler7 {
+    
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(9104);
+        Handler7 handler = new Handler7();
+        server.setHandler(new kilim.JettyHandler(handler));
+        server.start();
+    }
+
+    public static class Handler7 implements kilim.JettyHandler.Iface {
+        public String handle(String target,Request br,HttpServletRequest req,HttpServletResponse resp) throws Pausable,Exception {
+            Task.sleep(1000);
+            return "cruel world = " + req.getPathInfo();
+        }
+    }
+}
