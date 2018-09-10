d1cb829e99018e0da309c841ac8ae7c266ffc511
lytles@takashi <srl.source@nqzero.com>
2018-09-10 13:51:24 -0400

    jshell - add jshell demo

---
+++ demos/jshell/README.md
@@ -0,0 +1,60 @@
+demo of using kilim and runtime weaving with jshell, ie defining pausable methods inside jshell
+
+
+here's a transcript:
+
+
+```
+mvn compile
+cp=$(mvn -q dependency:build-classpath -Dmdep.outputFile=/dev/fd/1)
+$java9/bin/java -cp target/classes:$cp Kshell
+
+
+
+jshell> import kilim.*;
+
+jshell> Mailbox<Integer> mb = new Mailbox<>();
+mb ==> id:729803618 numMsgs:0
+
+jshell> Task.fork(() -> { for (int ii=0; ii < 5; ii++) { Task.sleep(1000); mb.put(ii); } })
+$39 ==> 24(running=true,pr=null)
+
+jshell> for (Integer val=0; val != null;) System.out.println(val = mb.getb(2000));
+0
+1
+2
+3
+4
+null
+
+jshell> Task.idledown()
+
+jshell> /exit
+|  Goodbye
+```
+
+
+notes:
+
+* jshell (not kilim) creates a timer thread that doesn't exit cleanly, and maven waits for it.
+use ctrl-c after exiting (or wait 15 seconds) when running with maven `exec:java`
+
+* the toplevel jshell method is not pausable (similar to `main()`) so it's not possible to call pausable methods directly
+
+* use `Task.fork` or `Task.spawn` or `new Task() {}.start()` to start a task
+
+* use blocking methods to access mailboxes and tasks, eg `mb.getb(2000)` or `task.joinb()`
+
+* calling pausable methods directly causes a `KilimException` and causes jshell to terminate. since this can be inconvenient, it may be handled more gracefully in the future
+
+* there's a java 10 bug that openjdk expects to fix in 10.3 (expected mid october) that affects some kilim lambdas. use a `-jdk10` versioned artifact in the meantime if you experience errors such as `java.lang.NoClassDefFoundError thrown: REPL/$JShell$17$$Lambda$437`
+
+
+
+
+
+
+
+
+
+
---
+++ demos/jshell/src/Kshell.java
@@ -0,0 +1,114 @@
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLConnection;
+import java.util.Map;
+import java.util.TreeMap;
+import jdk.jshell.execution.LoaderDelegate;
+import jdk.jshell.spi.ExecutionControl;
+import jdk.jshell.spi.ExecutionControl.ClassBytecodes;
+import jdk.jshell.spi.ExecutionControl.ClassInstallException;
+import jdk.jshell.spi.ExecutionControl.InternalException;
+import jdk.jshell.spi.ExecutionControlProvider;
+import jdk.jshell.spi.ExecutionEnv;
+import jdk.jshell.tool.JavaShellToolBuilder;
+import kilim.WeavingClassLoader;
+
+
+
+public class Kshell {
+    public static class Prov implements ExecutionControlProvider {
+        public String name() { return "kilim"; }
+
+        public ExecutionControl generate(ExecutionEnv arg0,Map<String,String> arg1) throws Throwable {
+            return new jdk.jshell.execution.DirectExecutionControl(new Loader().new Delegate());
+        }
+    }
+
+    public static class Loader extends URLClassLoader {
+        TreeMap<String,byte[]> map = new TreeMap();
+
+        Loader() {
+            super(new URL[0],Kshell.class.getClassLoader());
+        }
+
+        public InputStream getResourceAsStream(String name) {
+            byte [] bytes = map.get(name);
+            if (bytes != null) return new ByteArrayInputStream(bytes);
+            return super.getResourceAsStream(name);
+        }
+
+        class Delegate implements LoaderDelegate {
+            WeavingClassLoader wcl = new WeavingClassLoader(Loader.this,null);
+
+            public void load(ClassBytecodes[] codes) throws ClassInstallException {
+                for (int ii=0; ii < codes.length; ii++) {
+                    String name = codes[ii].name();
+                    String cname = WeavingClassLoader.makeResourceName(name);
+                    map.put(cname,codes[ii].bytecodes());
+                }
+                boolean [] installed = new boolean[codes.length];
+                for (int ii=0; ii < codes.length; ii++) {
+                    Class klass;
+                    try { klass = wcl.loadClass(codes[ii].name()); } catch (ClassNotFoundException ex) {
+                        throw new ClassInstallException("",installed);
+                    }
+                    installed[ii] = true;
+                }
+            }
+
+            public void classesRedefined(ClassBytecodes[] arg0) {}
+
+            public void addToClasspath(String cp) throws InternalException {
+                try {
+                    for (String path : cp.split(File.pathSeparator))
+                        addURL(new File(path).toURI().toURL());
+                }
+                catch (Exception ex) { throw new InternalException(ex.toString()); }
+            }
+
+            public Class<?> findClass(String name) throws ClassNotFoundException {
+                return wcl.loadClass(name);
+            }
+        }
+    }
+    
+    public static String buildClassPath(Class ... klasses) {
+        String cp = "";
+        for (Class klass : klasses) {
+            String name = klass.getName().replace(".","/") + ".class";
+            URL url = klass.getClassLoader().getResource(name);
+            String full = null;
+            try {
+                URLConnection open = url.openConnection();
+                if (open instanceof JarURLConnection) {
+                    JarURLConnection juc = (JarURLConnection) open;
+                    full = juc.getJarFile().getName();
+                }
+            }
+            catch (Exception ex) {}
+
+            if (full==null) try {
+                    String file = url.toURI().getPath();
+                    if (file.endsWith(name))
+                        full = file.substring(0,file.lastIndexOf(name));
+                }
+                catch (Exception ex) {
+                    throw new RuntimeException("unable to convert resource to filename: "
+                            + url);
+                }
+            cp += (cp.isEmpty() ? "":":") + full;
+        }
+        return cp;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        String cp = buildClassPath(kilim.Task.class,Kshell.class);
+        String [] config = new String[]{ "-execution", "kilim", "--class-path", cp };
+        JavaShellToolBuilder.builder().run(args.length > 0 ? args:config);
+    }
+}
